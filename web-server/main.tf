614f75aaa5f4b66f8e73741336b6b7e47bd32574
Jacob Tran <jacobntran@gmail.com>
2024-02-07 18:49:06 -0800

    configured ec2 instance to be a web server

--- web-server/main.tf
+++ web-server/main.tf
@@ -12,8 +12,28 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "web_server" {
     ami = data.aws_ami.ubuntu.id
     instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.web_server.id]
+
+    user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello, World" > index.html
+                nohup busybox httpd -f -p 8080 &
+                EOF
+
+    user_data_replace_on_change = true
 
     tags = {
         Name = "Ubuntu Web Server"
+    }
+}
+
+resource "aws_security_group" "web_server" {
+    name = "ec2-web-server-sg"
+
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
     }
 }
