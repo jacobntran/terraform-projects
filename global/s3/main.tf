a1c1fbc398b206e5d84ad481db65f0a3a273f593
Jacob Tran <jacobntran@gmail.com>
2024-02-13 17:25:30 -0800

    create s3 bucket for tf state

---
+++ global/s3/main.tf
@@ -0,0 +1,41 @@
+provider "aws" {
+    region = "us-west-1"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+    bucket = "jtran-terraform-state-bucket"
+
+    lifecycle {
+        prevent_destroy = true
+    }
+
+    tags = {
+        Name = "Terraform State"
+    }
+}
+
+resource "aws_s3_bucket_versioning" "enabled" {
+    bucket = aws_s3_bucket.terraform_state.id
+    
+    versioning_configuration {
+        status = "Enabled"
+    }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "enabled" {
+    bucket = aws_s3_bucket.terraform_state.id
+
+    rule {
+        apply_server_side_encryption_by_default {
+            sse_algorithm = "AES256"
+        }
+    }
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+    bucket = aws_s3_bucket.terraform_state.id
+    block_public_acls = true
+    block_public_policy = true
+    ignore_public_acls = true
+    restrict_public_buckets = true
+}
